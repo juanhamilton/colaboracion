2b45f1fbd71461583aec7d4576fba37cabc8788f
juanhamilton <51567942+juanhamilton@users.noreply.github.com>
2019-06-18 23:18:58 -0400

    Update my_js.js

--- js/my_js.js
+++ js/my_js.js
@@ -3,11 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $(".js-hidden").hide();
+});
 
 $(".feedback-toggler").click(function() {
   $id = $(this).attr("id");
   $idClass = "#sec-" + $id;
-  $($idClass).addClass("d-none");
+  $($idClass).addClass(".d-inline");
   
 })
 
