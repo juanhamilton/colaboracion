b2c14add7f4fe5e16c5599555e8a4e853d2fe349
juanhamilton <51567942+juanhamilton@users.noreply.github.com>
2019-06-18 22:56:28 -0400

    mejora

---
+++ js/my_js.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$("#boton1").click(function() {
+  alert( "Handler for .click() called." );
+})
+
